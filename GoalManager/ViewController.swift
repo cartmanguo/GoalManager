74396148b01af851de3626e61297fec95a80630f
cartmanguo <nggch1987@163.com>
2014-10-21 10:20:32 +0800

    Initial Commit

---
+++ GoalManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoalManager
+//
+//  Created by Line_Hu on 14-10-21.
+//  Copyright (c) 2014年 Alpha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
